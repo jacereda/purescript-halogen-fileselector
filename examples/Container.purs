e9e294468e72679e82dd6935b908eda0ad835d81
Jorge Acereda <jacereda@gmail.com>
2017-03-05 01:24:22 +0100

    Initial commit

---
+++ examples/Container.purs
@@ -0,0 +1,88 @@
+module Container where
+
+import Prelude
+import Halogen as H
+import Halogen.Component.ChildPath as CP
+import Halogen.FileSelector.Component as FileSelector
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Control.Monad.Aff (Aff)
+import DOM (DOM)
+import Data.Array (head)
+import Data.Const (Const)
+import Data.Maybe (Maybe(..), maybe)
+import Data.MediaType (MediaType(..))
+import Halogen.Component.ChildPath (type (\/), type (<\/>))
+import Halogen.Query (liftAff)
+
+type State = { txt :: Maybe String
+             , txts :: Array String
+             , img :: Maybe String
+             , imgs :: Array String
+             }
+
+type ChildQuery = FileSelector.Query <\/> FileSelector.Query <\/> FileSelector.Query <\/> FileSelector.Query <\/> Const Void
+type ChildSlot = FileSelector.Slot \/ FileSelector.Slot \/ FileSelector.Slot \/ FileSelector.Slot \/ Void
+type CompEff e = Aff (dom :: DOM | e)
+data Query a = HandleTextSelector FileSelector.Message a
+             | HandleTextsSelector FileSelector.Message a
+             | HandleImageSelector FileSelector.Message a
+             | HandleImagesSelector FileSelector.Message a
+
+component :: forall m. H.Component HH.HTML Query Unit Void (CompEff m)
+component =
+  H.parentComponent
+    { initialState: const { txt : Nothing
+                          , txts : []
+                          , img: Nothing
+                          , imgs: []
+                          }
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+  render :: State -> H.ParentHTML Query ChildQuery ChildSlot (CompEff m)
+  render state =
+    HH.div_ $ []
+    <> sel CP.cp1 HandleTextSelector [ HP.accept (MediaType "text/plain")
+                                     ]
+    <> maybeToArray (txt <$> state.txt)
+    <> sel CP.cp1 HandleTextsSelector [ HP.accept (MediaType "text/plain")
+                                      , HP.multiple true
+                                      ]
+    <> (txt <$> state.txts)
+    <> sel CP.cp3 HandleImageSelector [ HP.accept (MediaType "image/*")
+                                      , HP.capture true
+                                      ]
+    <> maybeToArray (img <$> state.img)
+    <> sel CP.cp4 HandleImagesSelector [ HP.accept (MediaType "image/*")
+                                       , HP.multiple true
+                                       , HP.capture true
+                                       ]
+    <> (img <$> state.imgs)    
+    where sel cp handler props = [ HH.div_ [ HH.slot' cp FileSelector.Slot (FileSelector.selector props) unit (HE.input handler) ] ]
+          txt c = HH.div_ [ HH.text c ]
+          img c = HH.div_ [ HH.img [ HP.src c ] ]
+          maybeToArray :: forall a. Maybe a -> Array a
+          maybeToArray = maybe [] pure
+
+  eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void (CompEff m)
+  eval (HandleTextSelector (FileSelector.Selected s) next) = do
+    contents <- liftAff $ FileSelector.texts s
+    H.modify $ (_ { txt = head contents })
+    pure next
+  eval (HandleTextsSelector (FileSelector.Selected s) next) = do
+    contents <- liftAff $ FileSelector.texts s
+    H.modify $ (_ { txts = contents })
+    pure next
+  eval (HandleImageSelector (FileSelector.Selected s) next) = do
+    contents <- liftAff $ FileSelector.urls s
+    H.modify $ (_ { img = head contents })
+    pure next
+  eval (HandleImagesSelector (FileSelector.Selected s) next) = do
+    contents <- liftAff $ FileSelector.urls s
+    H.modify $ (_ { imgs = contents })
+    pure next
---
+++ src/Halogen/FileSelector/Component.purs
@@ -0,0 +1,75 @@
+module Halogen.FileSelector.Component where
+
+import Prelude
+import Control.Monad.State as CMS
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Control.Monad.Aff (Aff)
+import Control.MonadZero (guard)
+import DOM (DOM)
+import DOM.File.FileList (item, length)
+import DOM.File.FileReader.Aff (readAsArrayBuffer, readAsDataURL, readAsText)
+import DOM.File.Types (Blob, FileList, fileToBlob)
+import DOM.HTML.Indexed (HTMLinput)
+import Data.Array ((..))
+import Data.ArrayBuffer.Types (ArrayBuffer)
+import Data.Maybe (Maybe(..), fromJust, isNothing)
+import Data.Nullable (toMaybe)
+import Data.Traversable (traverse)
+import Halogen.HTML.Properties (IProp)
+import Partial.Unsafe (unsafePartial)
+import Unsafe.Coerce (unsafeCoerce)
+
+type State = FileList
+
+data Query a  = Select FileList a
+              | GetSelection (State -> a)
+
+data Message = Selected FileList
+
+data Slot = Slot
+derive instance eqSlot :: Eq Slot
+derive instance ordSlot :: Ord Slot
+
+selector :: forall r m. Array (IProp HTMLinput r) -> H.Component HH.HTML Query Unit Message m
+selector props =
+  H.component
+    { initialState: const $ unsafeCoerce []
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+  render :: State -> H.ComponentHTML Query
+  render st = HH.input $ [ HP.type_ HP.InputFile
+                         , HE.onFilesSelected (HE.input Select)
+                         ] <> unsafeCoerce props
+
+  eval :: Query ~> H.ComponentDSL State Query Message m
+  eval (Select s next) = do
+    CMS.put s
+    H.raise $ Selected s
+    pure next      
+  eval (GetSelection reply) = do
+    s <- CMS.get
+    pure (reply s)
+
+blobs :: FileList -> Array Blob
+blobs fs = do
+  i <- 0 .. (length fs - 1)
+  let f = toMaybe $ item i fs
+  guard $ not $ isNothing f
+  pure $ fileToBlob $ unsafePartial $ fromJust f
+
+texts :: forall e. FileList -> Aff (dom :: DOM | e) (Array String)
+texts = traverse readAsText <<< blobs
+          
+urls :: forall e. FileList -> Aff (dom :: DOM | e) (Array String)
+urls = traverse readAsDataURL <<< blobs
+
+buffers :: forall e. FileList -> Aff (dom :: DOM | e) (Array ArrayBuffer)
+buffers = traverse readAsArrayBuffer <<< blobs
+
